5ba6e19493721e208e132911e71d7d422ee931d3
JordanFreitas <jordanfreitas4@gmail.com>
2016-03-01 21:58:16 -0800

    need to update dfs/bfs

--- ExploredGraph.java
+++ ExploredGraph.java
@@ -13,7 +13,7 @@ import java.util.function.Function;
  */
 
 /**
- * @author Anna Murray, UW NetID 1438929 and Jordan Freitas, 
+ * @author Anna Murray, UW NetID 1438929 and Jordan Freitas, UW NetID 1460037 
  * Extra Credit Options Implemented, if any:  (mention them here.)
  * 
  * Solution to Assignment 6 in CSE 373, Winter 2016
@@ -32,137 +32,183 @@ import java.util.function.Function;
 
 // Here is the main application class:
 public class ExploredGraph {
-	Set<Vertex> Ve; // collection of explored vertices
-	Set<Edge> Ee;   // collection of explored edges
-	
-	public ExploredGraph() {
-		Ve = new LinkedHashSet<Vertex>();
-		Ee = new LinkedHashSet<Edge>();
-	}
+    Set<Vertex> Ve; // collection of explored vertices
+    Set<Edge> Ee;   // collection of explored edges
+    
+    public ExploredGraph() {
+        Ve = new LinkedHashSet<Vertex>();
+        Ee = new LinkedHashSet<Edge>();
+    }
 
-	public void initialize() {
-		// Implement this
-	}
-	public int nvertices() {return 0;} // Implement this.
-	public int nedges() {return 0;}    // Implement this.
-	public void dfs(Vertex vi, Vertex vj) {} // Implement this.
-	public void bfs(Vertex vi, Vertex vj) {} // Implement this.
-	public ArrayList<Vertex> retrievePath(Vertex vi) {return null;} // Implement this.
-	public ArrayList<Vertex> shortestPath(Vertex vi, Vertex vj) {return null;} // Implement this.
-	public Set<Vertex> getVertices() {return Ve;} 
-	public Set<Edge> getEdges() {return Ee;} 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		ExploredGraph eg = new ExploredGraph();
-		// Test the vertex constructor: 
-		Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[]]");
-		System.out.println(v0);
-		// Add your own tests here.
-		// The autograder code will be used to test your basic functionality later.
+    public void initialize() {
+        Ve.clear();
+        Ee.clear();
+    }
+    
+    public void initialize(v) {
+        Ve.add(v);
+    }
+    
+    public int nvertices() {
+        return Ve.size();
+    } 
+    
+    public int nedges() {
+        return Ee.size();
+    }
+    
+    public void dfs(Vertex vi, Vertex vj) {
+        Vertex start = vi;
+        while(!vi.equals(vj)) || 
+    
+    
+    }
+    
+    private void dfsHelper(Vertex vi, Vertex vj) {
+        if vi.equals(vj) {
+        // dont do anything
+        }
+        if (!vi.equals(vj)) {
+            for (int i = 0; i <= 2; i++) {
+                for (int j = 0; j <= 2; j++) {
+                    Operator(i, j).getTransition
+        }
+        
+    }   
+    
+    public void bfs(Vertex vi, Vertex vj) {
+        //queue
+        Vertex start = vi;
+        while(!vi.equals(vj)) {
+           Operator temp = new
+            
+        }
+    }
+    
+    
+    public ArrayList<Vertex> retrievePath(Vertex vi) {return null;} // Implement this.
+    public ArrayList<Vertex> shortestPath(Vertex vi, Vertex vj) {return null;} // Implement this.
+    public Set<Vertex> getVertices() {return Ve;} 
+    public Set<Edge> getEdges() {return Ee;} 
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        ExploredGraph eg = new ExploredGraph();
+        // Test the vertex constructor: 
+        Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[]]");
+        System.out.println(v0);
+        // Add your own tests here.
+        // The autograder code will be used to test your basic functionality later.
 
-	}
-	
-	class Vertex {
-		ArrayList<Stack<Integer>> pegs; // Each vertex will hold a Towers-of-Hanoi state.
-		// There will be 3 pegs in the standard version, but more if you do extra credit option A5E1.
-		
-		// Constructor that takes a string such as "[[4,3,2,1],[],[]]":
-		public Vertex(String vString) {
-			String[] parts = vString.split("\\],\\[");
-			pegs = new ArrayList<Stack<Integer>>(3);
-			for (int i=0; i<3;i++) {
-				pegs.add(new Stack<Integer>());
-				try {
-					parts[i]=parts[i].replaceAll("\\[","");
-					parts[i]=parts[i].replaceAll("\\]","");
-					List<String> al = new ArrayList<String>(Arrays.asList(parts[i].split(",")));
-					System.out.println("ArrayList al is: "+al);
-					Iterator<String> it = al.iterator();
-					while (it.hasNext()) {
-						String item = it.next();
+    }
+    
+    class Vertex {
+        ArrayList<Stack<Integer>> pegs; // Each vertex will hold a Towers-of-Hanoi state.
+        // There will be 3 pegs in the standard version, but more if you do extra credit option A5E1.
+        
+        // Constructor that takes a string such as "[[4,3,2,1],[],[]]":
+        public Vertex(String vString) {
+            String[] parts = vString.split("\\],\\[");
+            pegs = new ArrayList<Stack<Integer>>(3);
+            for (int i=0; i<3;i++) {
+                pegs.add(new Stack<Integer>());
+                try {
+                    parts[i]=parts[i].replaceAll("\\[","");
+                    parts[i]=parts[i].replaceAll("\\]","");
+                    List<String> al = new ArrayList<String>(Arrays.asList(parts[i].split(",")));
+                    System.out.println("ArrayList al is: "+al);
+                    Iterator<String> it = al.iterator();
+                    while (it.hasNext()) {
+                        String item = it.next();
                                                 if (!item.equals("")) {
                                                         System.out.println("item is: "+item);
                                                         pegs.get(i).push(Integer.parseInt(item));
                                                 }
-					}
-				}
-				catch(NumberFormatException nfe) { nfe.printStackTrace(); }
-			}		
-		}
-		public String toString() {
-			String ans = "[";
-			for (int i=0; i<3; i++) {
-			    ans += pegs.get(i).toString().replace(" ", "");
-				if (i<2) { ans += ","; }
-			}
-			ans += "]";
-			return ans;
-		}
-	}
-	
-	class Edge {
-		public Vertex vi;
-		public Vertex vj;
-		
-		public Edge(Vertex vi, Vertex vj) {
-			this.vi = vi;
-			this.vj = vj;
-		}
-		
-		public Vertex getEndpoint1() {
-			return this.vi;
-		}
-		
-		public Vertex getEndpoint2() {
-			return this.vj;
-		}
-		
-		public String toString() {
-			return "Edge from " + vi.toString() + " to " + vj.toString();
-		}
-		
-		
-	}
-	
-	class Operator {
-		private int i, j;
+                    }
+                }
+                catch(NumberFormatException nfe) { nfe.printStackTrace(); }
+            }       
+        }
+        public String toString() {
+            String ans = "[";
+            for (int i=0; i<3; i++) {
+                ans += pegs.get(i).toString().replace(" ", "");
+                if (i<2) { ans += ","; }
+            }
+            ans += "]";
+            return ans;
+        }
+        
+        public boolean equals(Vertex v2) {
+            if (this.toString() == v2.toString()) {
+                return true;       
+            }
+            return false;    
+        }   
+    }
+    
+    class Edge {
+        public Vertex vi;
+        public Vertex vj;
+        
+        public Edge(Vertex vi, Vertex vj) {
+            this.vi = vi;
+            this.vj = vj;
+        }
+        
+        public Vertex getEndpoint1() {
+            return this.vi;
+        }
+        
+        public Vertex getEndpoint2() {
+            return this.vj;
+        }
+        
+        public String toString() {
+            return "Edge from " + vi.toString() + " to " + vj.toString();
+        }
+        
+        
+    }
+    
+    class Operator {
+        private int i, j;
 
-		public Operator(int i, int j) {
-			this.i = i;
-			this.j = j;
-		}
+        public Operator(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+        //Tests the validitiy of the move of a ring from peg i to j
+        public boolean getPrecondition(Vertex vertex) {
+            int ring1 = -1;
+            if (!vertex.pegs.get(i).isEmpty()) {
+                ring1 = vertex.pegs.get(i).peek();
+            }
+            int ring2 = -1;
+            if (!vertex.pegs.get(j).isEmpty()) {
+                ring2 = vertex.pegs.get(j).peek();
+            }
+            //Checks first to see if the first stack is empty otherwise compares the peeked values
+            if (ring1 = -1 || (ring2 != -1 && ring1 > ring2)) {
+                return false;
+            }
+            return true;                
+        }
 
-		Function<Vertex, Boolean> getPrecondition() {
-			// TODO: return a function that can be applied to a vertex (provided
-			// that the precondition is true) to get a "successor" vertex -- the
-			// result of making the move.
-			return new Function<Vertex, Boolean>() {
-				@Override
-				public Boolean apply(Vertex vertex) {
-					return false;
-				}
-			};
-		}
-
-		Function<Vertex, Vertex> getTransition() {
-			// TODO: should return a function that can be applied to a vertex
-			// (provided that the precondition is true) to get a "successor"
-			// vertex -- the result of making the move.
-			return new Function<Vertex, Vertex>() {
-				@Override
-				public Vertex apply(Vertex vertex) {
-					return null;
-				}
-			};
-		}
-
-		public String toString() {
-			// TODO: return a string good enough
-			// to distinguish different operators
-			return "";
-		}
-	}
+        //Moves the ring from peg i to j, dependent on passing the precondition, returning the new vertex
+        public Vertex getTransition(Vertex vertex) {
+            if(getPrecondition(vertex)){
+                vertex.pegs.get(j).push(vertex.pegs.get(i).pop());            
+            }
+            return vertex;
+            
+        }
+        
+        //Returns a string describing the change of rings on the pegs
+        public String toString() {
+            return "Move from peg " + i + " to " + j; 
+        }
+    }
 
 }
